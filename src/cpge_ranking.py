1bd9ae7bf68d9b24dcdd985fff567fd9a14f6e30
Thomas LABBE <thomas.labbe@orange.com>
2025-01-07 18:40:51 +0100

    Update cpge_ranking.py

--- src/cpge_ranking.py
+++ src/cpge_ranking.py
@@ -344,11 +344,11 @@ Comparez les établissements, estimez vos chances d’admission et optimisez vot
             st.session_state.quality_rate_weight = 0.8
             st.query_params = {"force": str(uuid.uuid4())}
 
-    apw_weight = st.slider("Acceptance Probability Weight : Probabilité d'admission selon sa moyenne générale (données 2024)",
+    apw_weight = st.slider("Acceptance Probability Weight : Probabilité d'admission selon sa moyenne générale",
                            min_value=0.0, max_value=1.0, step=0.05, key="apw_weight")
-    access_rate_weight = st.slider("Normalized Access Rate Weight : Sélectivité à l'accès (données 2023)",
+    access_rate_weight = st.slider("Normalized Access Rate Weight : Sélectivité à l'accès",
                                    min_value=0.0, max_value=1.0, step=0.05, key="access_rate_weight")
-    quality_rate_weight = st.slider("Normalized Quality Rate Weight : Taux de réussite aux concours (données 2024)",
+    quality_rate_weight = st.slider("Normalized Quality Rate Weight : Taux de réussite aux concours (moyenne sur 5 ans)",
                                     min_value=0.0, max_value=1.0, step=0.05, key="quality_rate_weight")
 
 
